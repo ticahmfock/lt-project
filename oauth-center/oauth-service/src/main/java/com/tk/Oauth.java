25f1aea87e0419a1db4b52686c566b0098ff9af9
tianchaofa <294962719@qq.com>
2020-10-23 16:17:27 +0800

    init commit

---
+++ oauth-center/oauth-service/src/main/java/com/tk/Oauth.java
@@ -0,0 +1,15 @@
+package com.tk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Oauth {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth.class, args);
+	}
+
+}
