c307133bb56f1485a51ca72800b48bd827480fdf
tianchaofa <294962719@qq.com>
2020-10-25 11:11:53 +0800

    init commit

--- register-center/eureka-slave/src/main/java/com/tk/Slave.java
+++ register-center/eureka-slave/src/main/java/com/tk/Slave.java
@@ -3,6 +3,9 @@ package com.tk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -12,4 +15,15 @@ public class Slave {
 		SpringApplication.run(Slave.class, args);
 	}
 
+  /**
+   * 开启安全认证
+   */
+  @EnableWebSecurity
+  public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.csrf().disable();
+      super.configure(http);
+    }
+  }
 }
